55c017dccd6f432412ed935ca293c8469cd372bc
Yukine <wiktorwlodarczyk321@gmail.com>
2025-03-05 15:44:57 +0100

    s

--- lesson2/tick_tack_toe.cpp
+++ lesson2/tick_tack_toe.cpp
@@ -29,24 +29,31 @@ bool isFinished(char board[3][3],char *winner){
         {
             if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] !=' '){
                 // cout<<board[i][0]<<" won"<<endl;
+                cout<<"war1"<<endl;
                 *winner = board[i][0];
                 return true;
                 break;
             }
             else if(board[0][j] == board[1][j]&& board[1][j] == board[2][j] &&board[1][j]!= ' '){
                 cout<<board[0][j]<<" won"<<endl;
+                cout<<"war2"<<endl;
+
                 *winner = board[0][j];
                 return true;
                 break;
             }
             else if(board[0][0] == board[1][1]&& board[1][1] == board[2][2] &&board[1][1] !=' '){
                 cout<<board[0][0]<<" won"<<endl;
+                cout<<"war3"<<endl;
+
                 *winner = board[0][0];
                 return true;
                 break;
             }
-            else if(board[0][2] == board[1][1]&& board[1][1] == board[0][2] &&board[1][1] !=' '){
+            else if(board[0][2] == board[1][1]&& board[1][1] == board[2][0] &&board[1][1] !=' '){
                 cout<<board[0][0]<<" won"<<endl;
+                cout<<"war4"<<endl;
+
                 *winner = board[0][0];
                 return true;
                 break;
@@ -100,7 +107,7 @@ int main(){
             }
         };
         showBoard(board);
-
+        cout<<turn<<endl;
         turn++;
         if(turn == 9){
             cout<<"Remis"<<endl;
