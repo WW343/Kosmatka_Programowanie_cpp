7a00a3c20efd0708b933046e2eb361a04f73c4da
Yukine <wiktorwlodarczyk321@gmail.com>
2025-05-07 13:11:29 +0200

    add lesson11

--- lesson11/canvas.cpp
+++ lesson11/canvas.cpp
@@ -40,3 +40,59 @@ public:
     Painter(Canvas& c) : canvas(c) {}
     virtual void paint() = 0;
 };
+
+
+    class VerticalLinePainter: public Painter{
+        int x;
+        public:
+        VerticalLinePainter(Canvas &c, size_t v): Painter(c), x(v){}
+
+        void paint() override {
+            for(int i = 0; i< canvas.getHeight();i++){
+                canvas.set(x,i);
+            }
+        }
+    };
+    class HorizontalLinePainter : public Painter{
+        int y;
+        public:
+            HorizontalLinePainter(Canvas &c, size_t b): Painter(c), y(b){}
+
+        void paint() override {
+            for(int i = 0; i< canvas.getWidth();i++){
+                canvas.set(i,y);
+            }
+        }
+    };
+
+    class RectanglePainter : public Painter{
+        public:
+        RectanglePainter(Canvas &c):Painter(c){}
+
+        void paint() override {
+            for(int i = 0; i< canvas.getHeight();i++){
+                canvas.set(0,i);
+                canvas.set(canvas.getWidth()-1,i);
+            }
+            for(int i = 0; i< canvas.getWidth();i++){
+                canvas.set(i,0);
+                canvas.set(i,canvas.getHeight()-1);
+
+            }
+        }
+    };
+
+
+int main(){
+    Canvas c(20,20);
+    VerticalLinePainter vline(c,2);
+    HorizontalLinePainter hline(c,2);
+    RectanglePainter rect(c);
+
+    vline.paint();
+    hline.paint();
+    rect.paint();
+
+    c.print();
+
+}
